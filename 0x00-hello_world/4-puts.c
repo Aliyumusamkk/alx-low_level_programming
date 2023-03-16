81dba35f5480d2a336309154801c4a8a160338ff
Aliyumusamkk <aliyumusakalakala@gmail.com>
2023-03-16 15:47:15 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * *main -msin block
+ *
+ * * Return: always 0
+ *
+ */
+
+int main(void)
+
+{
+
+	puts("\"gramming is like building a multilingual puzzle");
+
+	return (0);
+i
+}
